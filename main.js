1f8b3fe96103f8fc65c3efc15ffda2eb43cdaf26
Saadman Sayeed <786saadman@gmail.com>
2021-12-15 22:00:09 +0600

    js dom

---
+++ main.js
@@ -0,0 +1,18 @@
+/* DOM */
+      let a = "Hello";
+      let b = "World";
+    //   alert(a + " " + b);
+
+   
+    // document.getElementById("red").innerHTML = "Hello World!";
+   
+// console.log(document.getElementsByClassName("btn"))
+    
+
+// document.getElementsByClassName("btn")[0].innerHTML = "Hello World!";
+      
+// document.getElementsByClassName("btn")[1].innerHTML = "Hello World!";
+
+// document.getElementsByClassName("btn")[2].innerHTML = "Hello World!";
+
+// document.getElementsByClassName("btn")[3].innerHTML = "Hello World!";
