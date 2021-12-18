9d726b36f68382e541061af74e633e19e200c13f
Saadman Sayeed <786saadman@gmail.com>
2021-12-18 20:54:03 +0600

    class 1 done

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 /* DOM */
+/* EVENT HANDLING */
       let a = "Hello";
       let b = "World";
     //   alert(a + " " + b);
 
+
+// document.getElementsByTagName("h1")[0].innerHTML = "AMI VAT KHAI"
+
+// document.getElementsByTagName("h1")[1].innerHTML = "AMI VAT KHAI"
+
+// document.getElementsByTagName("h1")[2].innerHTML="AMI VAT KHAI"
    
     // document.getElementById("red").innerHTML = "Hello World!";
    
