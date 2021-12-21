44ae753db16769fb7950b4c1a54bc656bd7c4d19
Saadman Sayeed <786saadman@gmail.com>
2021-12-21 20:41:00 +0600

    class 2

--- main.js
+++ main.js
@@ -1,3 +1,114 @@
+/* Event Lister */
+
+// const changeBoxRedColor = () => {
+//     console.log("Red Clicked")
+//     document.getElementById("box").style.color = "#fff"
+//     document.getElementById("box").style.backgroundColor = "red"
+//     document.getElementById("box").innerText =
+//         "I am red box"
+// }
+
+// const changeBoxGreenColor = () => {
+//     console.log("Green Clicked")
+//     document.getElementById("box").style.color = "#fff"
+//     document.getElementById("box").style.backgroundColor = "green"
+//         document.getElementById("box").innerText =
+//         "I am green box"
+// }
+
+const changeBoxColor = (text, textColor, bgColor) => {
+    const confirmKey = confirm(`You have clicked ${bgColor}`);
+    
+
+    console.log(confirmKey);
+
+    /* back tick ` - tab er upore, esc er niche */
+    console.log(`I am  ${bgColor} clicked`)
+    
+    if (confirmKey == true) {
+       document.getElementById("box").style.color = textColor
+    document.getElementById("box").style.backgroundColor = bgColor
+    document.getElementById("box").innerText = text
+    } 
+
+
+    if (confirmKey == false) {
+        alert("Failed");
+    }
+    
+
+
+ 
+}
+
+
+/* Arrow function */
+
+// const studentName = "Lorin";
+// const dob = "31-12-2002";
+
+// const ageCalculator = (para1,para2) => {
+//    console.log(para1)
+//     console.log(para2)
+//     const d = new Date()
+
+//     const splitted_dob = para2.split("-");
+
+//     const usersDay = splitted_dob[0];
+//     const usersMonth = splitted_dob[1];
+//     const usersYear = splitted_dob[2];
+
+//     console.log(splitted_dob)
+
+//     const year = d.getFullYear()
+//     const month = d.getMonth()
+//     const date = d.getDate()
+    
+//     console.log(year + "/" + month + "/" + date);
+
+//     console.warn('Age of ' + para1)
+//     const age = year - usersYear + " year";
+//     return age
+
+// }
+
+
+// const getAge = ageCalculator(studentName, dob)
+// console.log(getAge)
+
+/* Function */
+// function ageCalculator(para1,para2) {
+//     console.log(para1)
+//     console.log(para2)
+//     const d = new Date()
+
+//     const splitted_dob = para2.split("-");
+
+//     const usersDay = splitted_dob[0];
+//     const usersMonth = splitted_dob[1];
+//     const usersYear = splitted_dob[2];
+
+//     console.log(splitted_dob)
+
+//     const year = d.getFullYear()
+//     const month = d.getMonth()
+//     const date = d.getDate()
+    
+//     console.log(year + "/" + month + "/" + date);
+
+//     console.warn('Age of ' + para1)
+//     const age = year - usersYear + " year";
+//     return age
+
+// }
+
+
+// const getAge = ageCalculator(studentName, dob);
+
+// console.log('getAge = ' + getAge);
+
+
+
 /* DOM */
 /* EVENT HANDLING */
       let a = "Hello";
